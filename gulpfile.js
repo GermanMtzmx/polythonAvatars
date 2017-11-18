4af9ec13414e060aa647931a240bc5591efae825
CarlsKe <fernancarldiaz@outlook.com>
2017-11-18 15:53:42 -0600

    feat: added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+ 
+gulp.task('mincss', function () {
+    gulp.src('src/css/develop/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('src/css/dist'));
+});
+
